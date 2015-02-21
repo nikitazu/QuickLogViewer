42556090e2e8e83ebfd7178693c06fc99eec6efc
Nikita B. Zuev <nikitazu@gmail.com>
2015-02-21 04:57:29 +0300

    Refactoring (missing file added)

---
+++ QuickLogViewer/Components/CsharpStacktraceFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickLogViewer.Components
+{
+    public class CsharpStacktraceFormatter
+    {
+        private static readonly Rtf _rtf = new Rtf();
+
+        public string TryFormatCsharpStacktrace(string text)
+        {
+            if (!HasCsharpStacktrace(text))
+            {
+                return text;
+            }
+
+            return _rtf.RichText(InjectCsharpStacktraceNewlines(text));
+        }
+
+        private string InjectCsharpStacktraceNewlines(string text)
+        {
+            return text
+                .Replace("Exception:", "Exception:" + Rtf.NewLine + Rtf.Red)
+                .Replace(" at ", Rtf.NewLine + Rtf.Gray + " -> ");
+        }
+
+        private bool HasCsharpStacktrace(string text)
+        {
+            return text.Contains("Exception") && text.Contains(" at ") && text.Contains(".cs:line");
+        }
+    }
+}
