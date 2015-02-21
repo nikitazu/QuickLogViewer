e49e69da8cc0d2bfeba2e507a84a00b3dd33dc2b
Nikita B. Zuev <nikitazu@gmail.com>
2015-02-21 04:12:11 +0300

    Displaying logs with rich text editor, to enable further formatting

---
+++ QuickLogViewer/UI/RichTextBoxHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents; 
+
+namespace QuickLogViewer.UI
+{
+    public class RichTextBoxHelper : DependencyObject
+    {
+        public static string GetDocumentRtf(DependencyObject obj)
+        {
+            return (string)obj.GetValue(DocumentRtfProperty);
+        }
+
+        public static void SetDocumentRtf(DependencyObject obj, string value)
+        {
+            obj.SetValue(DocumentRtfProperty, value);
+        }
+
+        public static readonly DependencyProperty DocumentRtfProperty =
+            DependencyProperty.RegisterAttached("DocumentRtf", typeof(string), typeof(RichTextBoxHelper), new FrameworkPropertyMetadata
+            {
+                BindsTwoWayByDefault = false,
+                PropertyChangedCallback = (sender, e) =>
+                    {
+                        var richTextBox = (RichTextBox)sender;
+
+                        var rtf = string.Format("{0}", GetDocumentRtf(richTextBox));
+                        var doc = new FlowDocument();
+                        var range = new TextRange(doc.ContentStart, doc.ContentEnd);
+
+                        range.Load(new MemoryStream(Encoding.UTF8.GetBytes(rtf)), DataFormats.Rtf);
+                        richTextBox.Document = doc;
+                    }
+            });
+    }
+}
