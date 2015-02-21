458aa2ba3d211e1f1f24f4df2b33e46b82440b8d
Nikita B. Zuev <nikitazu@gmail.com>
2015-02-21 05:06:05 +0300

    Updated metadata

--- QuickLogViewer/Properties/AssemblyInfo.cs
+++ QuickLogViewer/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QuickLogViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows even log viewer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("nikitazu")]
 [assembly: AssemblyProduct("QuickLogViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Nikita B. Zuev 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
